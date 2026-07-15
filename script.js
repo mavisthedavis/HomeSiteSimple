c84b8001326fb1d8235e06e83f1883bfce95c099
Mavis Davis <mavisledavis563@gmail.com>
2026-07-15 15:46:38 -0500

    Fix jQuery selector syntax in imageUpdate function

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ class imageInfo {
   
 }  
 function imageUpdate() {
-  ("img").attr("src", image.getSrc());   
+  $("img").attr("src", image.getSrc());   
 }
 window.onload = function() {  
   image = new imageInfo(["", "https://pbs.twimg.com/profile_images/1799909814660689921/osCrUSI7_400x400.jpg", "https://static.wikia.nocookie.net/unbeatable/images/b/bc/Quaver_Characterdesign.png/revision/latest/smart/width/250/height/250?cb=20220425214443"], 0); 
