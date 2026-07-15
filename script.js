1b07d844eca02421018be4ad08870d44485ce7a5
Mavis Davis <mavisledavis563@gmail.com>
2026-07-15 15:32:12 -0500

    Implement imageUpdate class for image management

---
+++ script.js
@@ -0,0 +1,24 @@
+class imageUpdate { 
+  constructor(imageList, index) { 
+    this.imageList = imageList; 
+    this.index = index; 
+  }  
+  updateIndex(index) { 
+    this.index = index; 
+  }  
+  getSrc() { 
+    return imageList[index]; 
+  }
+  printValues() { 
+    console.log(imageList); 
+    console.log(index); 
+  } 
+  
+}  
+function imageUpdate(); 
+  ("img").attr("src", image.getSrc());   
+}
+window.onload = function() {  
+  image = imageUpdate(["", "https://pbs.twimg.com/profile_images/1799909814660689921/osCrUSI7_400x400.jpg", "https://static.wikia.nocookie.net/unbeatable/images/b/bc/Quaver_Characterdesign.png/revision/latest/smart/width/250/height/250?cb=20220425214443"], 0); 
+  imageUpdate(); 
+} 
