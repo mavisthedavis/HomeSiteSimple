eaced0dd422cdfe17d6628cf34341420c62a4f86
Mavis Davis <mavisledavis563@gmail.com>
2026-07-15 15:59:06 -0500

    Enhance image update logic and event handling
    Refactor image update functions and event handlers.

--- script.js
+++ script.js
@@ -16,10 +16,34 @@ class imageInfo {
     console.log(this.#index); 
   } 
   
-}  
+}   
+
 function imageUpdate() {
   $("img").attr("src", image.getSrc());   
-}
+} 
+
+const ElmList = [$("#youtube"), $("#bsky"), $("#github"), $("#contact")]; 
+func setUp() { 
+  for (let i = 0; i < ElmList.length; i++) { 
+    ElmList.on("mouseenter", Enter);  
+    ElmList.on("mouseenter", Leave);  
+  } 
+} 
+
+function Enter(this) { 
+  for (let i = 0; i < ElmList.length; i++) { 
+    if ($(this) == ElmList[i]) { 
+      image.updateIndex(i); 
+      imageUpdate(); 
+    }
+  } 
+} 
+
+function Leave() { 
+  image.updateIndex(0); 
+  imageUpdate();  
+} 
+
 window.onload = function() {  
   image = new imageInfo(["", "https://pbs.twimg.com/profile_images/1799909814660689921/osCrUSI7_400x400.jpg", "https://static.wikia.nocookie.net/unbeatable/images/b/bc/Quaver_Characterdesign.png/revision/latest/smart/width/250/height/250?cb=20220425214443"], 0); 
   imageUpdate(); 
