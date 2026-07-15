aec94fa4c89a02cb13226c031bf78587f0511b3e
Mavis Davis <mavisledavis563@gmail.com>
2026-07-15 15:39:25 -0500

    Instantiate imageInfo with 'new' keyword

--- script.js
+++ script.js
@@ -19,6 +19,6 @@ function imageUpdate() {
   ("img").attr("src", image.getSrc());   
 }
 window.onload = function() {  
-  image = imageInfo(["", "https://pbs.twimg.com/profile_images/1799909814660689921/osCrUSI7_400x400.jpg", "https://static.wikia.nocookie.net/unbeatable/images/b/bc/Quaver_Characterdesign.png/revision/latest/smart/width/250/height/250?cb=20220425214443"], 0); 
+  image = new imageInfo(["", "https://pbs.twimg.com/profile_images/1799909814660689921/osCrUSI7_400x400.jpg", "https://static.wikia.nocookie.net/unbeatable/images/b/bc/Quaver_Characterdesign.png/revision/latest/smart/width/250/height/250?cb=20220425214443"], 0); 
   imageUpdate(); 
 } 
