4e1da5bb22351d5dfff0a53ee0b3b2d7febe07c3
Mavis Davis <mavisledavis563@gmail.com>
2026-07-15 19:17:56 -0500

    Refactor fade in/out logic and variable initialization

--- script.js
+++ script.js
@@ -30,28 +30,53 @@ function setUp() {
   } 
 }  
 var inverval_wait = 50; 
-var entered = false;  
+var entered = false;   
+var FadeInAndOut == null; 
 function Enter() {  
   entered = true;  
   index = 0; 
   for (let i = 0; i < ElmList.length; i++) {  
     //console.log($(this)); 
     //console.log(ElmList[i]); 
-    if (this.id == ElmList[i].attr('id')) {    
-      fadeInInterval = setInterval(fadeIn, inverval_wait);   
+    if (this.id == ElmList[i].attr('id')) {     
+      if (FadeInAndOut == null) { 
+        FadeInAndOut = setInterval(fade, inverval_wait);    
+      }
       image.updateIndex(i); 
       imageUpdate();
        
     }
   } 
-} 
+}  
 var fadeOutInterval;
-function Leave() {   
-   fadeOutInterval = setInterval(fadeOut, inverval_wait);
+function Leave() {    
+   entered = false;  
+   if(FadeInAndOut == null) { 
+     FadeInAndOut = setInterval(fade, inverval_wait); 
+   }
  
 }  
 var numb_change = 0.09; 
-var temp_numb = false;
+var temp_numb = false; 
+
+function fade() { 
+  if (entered == true) { 
+    temp_numb += numb_change;  
+  } else { 
+    temp_numb -= numb_change; 
+  } 
+  $("#image").css("opacity", temp_numb);   
+
+  if (temp_numb > 1) {  
+    clearInterval(FadeInAndOut); 
+    temp_numb = 1;  
+    FadeInAndOut = null; 
+  } else if (temp_numb < 0) { 
+    clearInterval(FadeInAndOut); 
+    temp_numb = 0; 
+    FadeInAndOut = null; 
+  }
+}
 function fadeIn() {   
   if(temp_numb == false && fadeOutInterval == null) { 
     temp_numb = 0.1;  
